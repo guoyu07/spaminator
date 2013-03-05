2e533690e68ab2125ea1ece655c8e5caf8bcff16
Jeff Tickle <jtickle@gmail.com>
2013-03-05 09:36:52 -0500

    Reorganization continues

--- js/spaminator/spaminator.js
+++ js/spaminator/spaminator.js
@@ -3,11 +3,13 @@ define([
     'underscore',
     'backbone',
     'bootstrap',
+    'spaminator/view/main',
 ], function($, _, Backbone, Bootstrap){
     var initialize = function() {
         $('#spaminator').css({
             'margin-bottom': $('#footer').outerHeight(true),
         });
+
     }
 
     return {
