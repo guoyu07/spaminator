6109b24d992f971586e58b717b8ca69de7945b00
Chris Coley <chris.num2@gmail.com>
2013-08-30 09:01:22 -0400

    Still working on the templates for the population and emailTemplate pages.

--- app/scripts/controllers/Population.js
+++ app/scripts/controllers/Population.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('spaminatorApp')
-  .controller('PopulationCtrl', function ($scope) {
+  .controller('PopulationCtrl', function ($scope, UserView) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    //UserView.setViewable("Template");
+    $scope.allowNext = function() {
+        return $scope.population.length > 0 ? true : false;
+    }
+
+    // Test data
+    $scope.population = [["900458187","Coley","Christopher","Jacob","","","coleycj","coleycj@appstate.edu"],
+                         ["000000000","McTester","Test","Reginald","Sir","III","mctestertr","mctestertr@appstate.edu"]];
   });
--- app/scripts/controllers/Template.js
+++ app/scripts/controllers/Template.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('spaminatorApp')
-  .controller('TemplateCtrl', function ($scope) {
+  .controller('TemplateCtrl', function ($scope, UserView) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    UserView.setViewable('Sender');
   });
