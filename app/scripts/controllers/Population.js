970bb45ac2ad38e53daa9f539512e4ce7c06fed5
Chris Coley <chris.num2@gmail.com>
2013-10-16 13:13:50 -0400

    Probably the last commit to this.

--- app/scripts/controllers/Population.js
+++ app/scripts/controllers/Population.js
@@ -2,11 +2,15 @@
 
 angular.module('spaminatorApp')
   .controller('PopulationCtrl', function ($scope, UserView, $http) {
-    // Set the Population tab viewable
+    // Set the current tab viewable
     UserView.setViewable("Population");
 
     // Population array
     $scope.population = [];
+
+    // We use the Banner ID to keep duplicates out of the population
+    // This array holds the Banner ID of every current population member
+    $scope.popIds = [];
 
     // Determine if the next step should be visible
     $scope.allowNext = function() {
@@ -16,24 +20,33 @@ angular.module('spaminatorApp')
     // Remove a member from the population array
     $scope.remove = function(index) {
         $scope.population.splice(index,1);
+
+        // Also remove their id from the popIds array so they can be re-added later.
+        $scope.popIds.splice(index,1);
     }
 
     // Submit population modal 
     $scope.popEntrySubmit = function(memberInfo) { 
 
         if (memberInfo) {
-            console.log("Doing it");
             //$http.post("scripts/getPopulation.php", memberInfo)     // Replace the next line with this line for production
             $http.post("http://blackfoot.appstate.edu/~chris/spaminator/app/scripts/getPopulation.php", memberInfo)  // Remove in production
                 .success(function(popData, status, headers, config) {
                     // Push each person into the population array
                     popData.forEach(function(entry) {
-                        $scope.population.push([entry["id"],entry["lname"],entry["fname"],"","","","",""]);
-                        //TODO: need a better way to fill in missing info
+                        // Only add people to the population if they are not already in it
+                        if ($.inArray(entry["id"], $scope.popIds) == -1) {
+                            $scope.population.push([entry["id"],entry["lname"],entry["fname"],"","","","",""]);
+                            $scope.popIds.push(entry["id"]);
+                            //TODO: need a better way to fill in missing info
+                        }
                     });
 
                     // Empty the modal
                     $scope.popEntryData="";
+
+                    // Set the Template tab as viewable
+                    UserView.setViewable("Template");
                 })
                 .error(function(popData, status, headers, config) {
                     console.log("ERROR--------------------------------");
@@ -51,4 +64,10 @@ angular.module('spaminatorApp')
         $scope.popEntryData="";
         $('#add-population-modal').modal('hide');
     };
+
+    // Focus on the textarea when the modal is shown 
+    $('#add-population-modal').on('shown', function() {
+        $('#add-population-modal textarea').focus();
+    })
   });
+
--- app/scripts/controllers/Template.js
+++ app/scripts/controllers/Template.js
@@ -2,11 +2,30 @@
 
 angular.module('spaminatorApp')
   .controller('TemplateCtrl', function ($scope, UserView) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
+    // Set the current tab viewable
     UserView.setViewable('Sender');
+
+    // Array of templates
+    $scope.templates = [];
+    $scope.templatesLoaded = true;
+    $scope.editTemplate = false;
+    $scope.activeTemplate;
+
+    // Test template
+    var testTemplate = {};
+    testTemplate.name = "Test Template";
+    testTemplate.template = "This is a test template for an email.";
+
+    // Populate the templates array with test data
+    $scope.templates.push(testTemplate);
+
+    $scope.loadTemplate = function(index) {
+        $scope.editTemplate = true;
+        $scope.activeTemplate = $scope.templates[index];
+    }
+
+    $scope.newTemplate = function() {
+        $scope.editTemplate = true;
+        console.log("Creating new template");
+    }
   });
